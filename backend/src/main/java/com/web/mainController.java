b085bca7f331b105f4620b050033b1e03747cb39
rbrtknwls <r2knowle@uwaterloo.ca>
2022-11-27 13:38:51 -0500

    controller setup

---
+++ backend/src/main/java/com/web/mainController.java
@@ -0,0 +1,26 @@
+package com.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+
+@Controller
+public class mainController {
+
+    @RequestMapping(value = "/getInfo")
+    public ModelAndView getDateAndTime() {
+
+        var now = LocalDateTime.now();
+        var dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        var date_time = dtf.format(now);
+
+        var params = new HashMap<String, Object>();
+        params.put("date_time", date_time);
+
+        return new ModelAndView("showMessage", params);
+    }
+}
