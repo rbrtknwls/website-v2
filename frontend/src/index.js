816fc35df3ebc309d576f90957e6267eb02d28da
rbrtknwls <r2knowle@uwaterloo.ca>
2022-11-28 02:31:49 -0500

    Updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './app';
-import './resources/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import './resources/index.css'
 
 const root = createRoot(document.getElementById('root'));
